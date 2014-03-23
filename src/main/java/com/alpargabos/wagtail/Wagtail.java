0fdefd965a37c12e6accb671d548743686210018
alpar.gabos <alpar.gabos@prezi.com>
2014-03-23 20:59:26 +0100

    wagtail skeleton

--- src/main/java/com/alpargabos/wagtail/Wagtail.java
+++ src/main/java/com/alpargabos/wagtail/Wagtail.java
@@ -1,13 +1,25 @@
 package com.alpargabos.wagtail;
 
+import java.io.*;
+
 public class Wagtail {
-    public Wagtail(User user) {
+
+    private Writer consoleOut;
+    private BufferedReader consoleIn;
+
+    public Wagtail() {
+        consoleOut = new PrintWriter(System.out);
     }
 
-    public void login() {
-    }
-
-    public boolean isConnected() {
+    public boolean login() {
         return false;
+    }
+
+    public void setUI(OutputStream output) {
+        consoleOut = new PrintWriter(output);
+    }
+
+    public void setInput(InputStream input) {
+        consoleIn = new BufferedReader(new InputStreamReader(System.in));
     }
 }
